23ab609b8d9bf45113fab3528311572bcb631075
jaydenswe <247421157+jaydenswe@users.noreply.github.com>
2026-04-13 18:36:10 +0100

    Added functionality to add multiple square divs

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+let times = 0;
+while (times < 256) {
 const container = document.getElementById("container");
-console.log(container);
 const div = document.createElement("div");
 div.classList.add("square");
 container.appendChild(div);
+times++;
+}
