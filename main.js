7af0feed0bfe7b675e5e14eef5da072744eca117
jaydenswe <247421157+jaydenswe@users.noreply.github.com>
2026-04-13 18:38:44 +0100

    Moved code into a function

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let times = 0;
-while (times < 256) {
-const container = document.getElementById("container");
-const div = document.createElement("div");
-div.classList.add("square");
-container.appendChild(div);
-times++;
+function createGrid() {
+  let times = 0;
+  const container = document.getElementById("container");
+  while (times < 256) {
+  const div = document.createElement("div");
+  div.classList.add("square");
+  container.appendChild(div);
+  times++;
+  }
 }
+createGrid();
