38dfebe07322312cc5d8542393d7d3dd31f79160
jaydenswe <247421157+jaydenswe@users.noreply.github.com>
2026-04-13 18:24:51 +0100

    Added functionality to add a single div

---
+++ main.js
@@ -0,0 +1,5 @@
+const container = document.getElementById("container");
+console.log(container);
+const div = document.createElement("div");
+div.classList.add("square");
+container.appendChild(div);
